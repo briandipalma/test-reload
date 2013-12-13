1467f19dac36f1e690b9e4a8df2b391b796fd69f
Brian Di Palma <offler@gmail.com>
2013-12-13 11:14:45 +0000

    Adding override of F5 functionality.

--- reload-test.js
+++ reload-test.js
@@ -1,4 +1,19 @@
 
+function setUpOverridesToBrowserBehaviour () {
+	function disableF5(event) {
+		if ((event.which || event.keyCode) === 116 || (event.which || event.keyCode) === 82) {
+			event.preventDefault();
+			showConfirm();
+		}
+	};
+
+	if(document.body.addEventListener) {
+		document.body.addEventListener("keydown", disableF5);
+	} else {
+		document.body.attachEvent("keydown", disableF5);
+	}
+}
+
 function showConfirm () {
 	var confirmHolder = document.getElementById("confirm_holder");
 	confirmHolder.style.display = "inherit";
